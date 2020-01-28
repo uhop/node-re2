194837cec31c4cb2140b67bff6f3ec8841ff6755
kobelb <brandon.kobel@elastic.co>
2020-01-28 13:22:20 -0800

    Let's try this again, got the paths wrong

--- build-resources/linux/build.sh
+++ build-resources/linux/build.sh
@@ -3,6 +3,6 @@
 PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]')
 echo "Building archives for $PACKAGE_VERSION"
 docker pull kobelb/build-node-re2:alpha-3
-docker run -t --name build --mount type=bind,source="$TRAVIS_BUILD_DIR",destination=/opt/app-root/src/node-re2 kobelb/build-node-re2:alpha-3 /bin/bash -c "cp -r ./node-re2 ./tmp; cd ./tmp; npm install"
+docker run -t --name build --mount type=bind,source="$(pwd)",destination=/opt/app-root/src/node-re2 kobelb/build-node-re2:alpha-3 /bin/bash -c "cp -r ./node-re2 ./tmp; cd ./tmp; npm install"
 mkdir -p ./target/
 docker cp build:/opt/app-root/src/tmp/build/Release/re2.node ./target/
