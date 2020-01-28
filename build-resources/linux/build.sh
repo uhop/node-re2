be9e44bd80d1c7999e14ab68fd24a92c66e35704
kobelb <brandon.kobel@elastic.co>
2020-01-28 13:37:20 -0800

    Making the target directory correctly

--- build-resources/linux/build.sh
+++ build-resources/linux/build.sh
@@ -2,5 +2,4 @@
 
 docker pull kobelb/build-node-re2:alpha-3
 docker run -t --name build --mount type=bind,source="$(pwd)",destination=/opt/app-root/src/node-re2 kobelb/build-node-re2:alpha-3 /bin/bash -c "cp -r ./node-re2 ./tmp; cd ./tmp; npm install"
-mkdir -p ./target/
 docker cp build:/opt/app-root/src/tmp/build/Release/re2.node ./target/
