09db138aaa17ef0c399566f3b9a38abfedbdb1f5
kobelb <brandon.kobel@elastic.co>
2020-03-13 11:38:27 -0700

    Adjusting travis and build for node-pre-gyp

--- build-resources/linux/build.sh
+++ build-resources/linux/build.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 docker pull kobelb/build-node-re2:alpha-3
-docker run -t --name build --mount type=bind,source="$(pwd)",destination=/opt/app-root/src/node-re2 kobelb/build-node-re2:alpha-3 /bin/bash -c "cp -r ./node-re2 ./tmp; cd ./tmp; npm install"
-docker cp build:/opt/app-root/src/tmp/build/Release/re2.node ./target/
+docker run -t --name build --mount type=bind,source="$(pwd)",destination=/opt/app-root/src/node-re2 kobelb/build-node-re2:alpha-3 /bin/bash -c "cp -r ./node-re2 ./tmp; cd ./tmp; npm install --build-from-source"
+docker cp build:/opt/app-root/src/tmp/binding/re2.node ./binding/
