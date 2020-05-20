536debb0fc7961f5fa5e6d0359959b6e22bfdf8c
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2020-05-20 12:39:37 -0500

    Improved the worker test.

--- tests/worker.js
+++ tests/worker.js
@@ -7,17 +7,23 @@ const RE2 = require('../re2');
 if (isMainThread) {
   // This re-loads the current file inside a Worker instance.
   console.log('Inside Master!');
-  new Worker(__filename);
-  test();
+  const worker = new Worker(__filename);
+  worker.on('exit', code => {
+    console.log('Exit code:', code);
+    test('#2');
+  });
+  test('#1');
 } else {
   console.log('Inside Worker!');
   test();
 }
 
-function test() {
+function test(msg) {
+  msg && console.log(isMainThread ? 'Main' : 'Worker', msg);
+
   const a = new RE2('^\\d+$');
-  console.log(isMainThread, a.test('123'), a.test('abc'), a.test('123abc'));
+  console.log(isMainThread, a.test('123'), a.test('abc'), a.test('123abc'), a instanceof RE2);
 
   const b = RE2('^\\d+$');
-  console.log(isMainThread, b.test('123'), b.test('abc'), b.test('123abc'));
+  console.log(isMainThread, b.test('123'), b.test('abc'), b.test('123abc'), b instanceof RE2);
 }
