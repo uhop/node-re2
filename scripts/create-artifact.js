547d58f518e06925c837ee3b553a368992e3bac2
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2020-05-24 00:25:09 -0500

    Realistic uploader.

--- scripts/create-artifact.js
+++ scripts/create-artifact.js
@@ -1,17 +1,60 @@
 'use strict';
 
+const {promises: fsp} = require('fs');
+const path = require('path');
+const zlib = require('zlib');
+const {promisify} = require('util');
+
 const github = require('@actions/github');
 
-const TOKEN = process.env.GITHUB_TOKEN,
-  REF = process.env.REF;
+const OWNER = 'uhop',
+  REPO = 'node-re2';
 
-console.log(REF, process.platform, process.arch, process.versions.modules);
+const TOKEN = process.env.GITHUB_TOKEN,
+  REF = process.env.REF || '',
+  TAG = /^refs\/tags\/(.*)$/.exec(REF)[1];
+
+const fileName = `re2-${process.platform}-${process.arch}-${process.versions.modules}.node`;
+
+console.log('Preparing artifact', fileName, '...');
 
 const octokit = new github.GitHub(TOKEN);
 
 const main = async () => {
-  const response = await octokit.repos.listReleases({owner: 'uhop', repo: 'node-re2'});
-  console.log(response);
+  const [data, uploadUrl] = await Promise.all([
+    fsp.readFile(path.join(__dirname, '../build/Release/re2.node')),
+    octokit.repos.getReleaseByTag({owner: OWNER, repo: REPO, tag: TAG}).then(response => response.data.upload_url)
+  ]);
+  console.log('Compressing and uploading ...');
+  await Promise.all([
+    (async () => {
+      if (!zlib.brotliCompress) return null;
+      const compressed = await promisify(zlib.brotliCompress)(data, {params: {[zlib.constants.BROTLI_PARAM_QUALITY]: zlib.constants.BROTLI_MAX_QUALITY}});
+      return octokit.repos
+        .uploadReleaseAsset({
+          url: uploadUrl,
+          data: compressed,
+          name: fileName + '.br',
+          headers: {'content-type': 'application/brotli', 'content-length': compressed.length}
+        })
+        .then(() => console.log('Uploaded BR.'))
+        .catch(() => console.log('BR has failed to upload.'));
+    })(),
+    (async () => {
+      if (!zlib.gzip) return null;
+      const compressed = await promisify(zlib.gzip)(data, {level: zlib.constants.Z_BEST_COMPRESSION});
+      return octokit.repos
+        .uploadReleaseAsset({
+          url: uploadUrl,
+          data: compressed,
+          name: fileName + '.gz',
+          headers: {'content-type': 'application/gzip', 'content-length': compressed.length}
+        })
+        .then(() => console.log('Uploaded GZ.'))
+        .catch(() => console.log('GZ has failed to upload.'));
+    })()
+  ]);
+  console.log('Done.');
 };
 
 main();
