dd9c8c308484acf7b5a4699a37c135d5833dc0a1
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2020-05-24 20:42:03 -0500

    Copied create-artifact to an action.

---
+++ .github/actions/create-binary-artifact/index.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const {promises: fsp} = require('fs');
+const path = require('path');
+const zlib = require('zlib');
+const {promisify} = require('util');
+
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+const OWNER = core.getInput('owner'),
+  REPO = core.getInput('repo'),
+  TOKEN = process.env.GITHUB_TOKEN,
+  PATH = core.getInput('path'),
+  REF = core.getInput('tag'),
+  TAG = /^refs\/tags\/(.*)$/.exec(REF)[1];
+
+const fileName = `re2-${process.platform}-${process.arch}-${process.versions.modules}.node`;
+
+console.log('Preparing artifact', fileName, '...');
+
+const octokit = new github.GitHub(TOKEN);
+
+const main = async () => {
+  const [data, uploadUrl] = await Promise.all([
+    fsp.readFile(path.normalize(PATH)),
+    octokit.repos.getReleaseByTag({owner: OWNER, repo: REPO, tag: TAG}).then(response => response.data.upload_url)
+  ]);
+  console.log('Compressing and uploading ...');
+  await Promise.all([
+    (async () => {
+      if (!zlib.brotliCompress) return null;
+      const compressed = await promisify(zlib.brotliCompress)(data, {params: {[zlib.constants.BROTLI_PARAM_QUALITY]: zlib.constants.BROTLI_MAX_QUALITY}});
+      return octokit.repos
+        .uploadReleaseAsset({
+          url: uploadUrl,
+          data: compressed,
+          name: fileName + '.br',
+          headers: {'content-type': 'application/brotli', 'content-length': compressed.length}
+        })
+        .then(() => console.log('Uploaded BR.'))
+        .catch(() => console.log('BR has failed to upload.'));
+    })(),
+    (async () => {
+      if (!zlib.gzip) return null;
+      const compressed = await promisify(zlib.gzip)(data, {level: zlib.constants.Z_BEST_COMPRESSION});
+      return octokit.repos
+        .uploadReleaseAsset({
+          url: uploadUrl,
+          data: compressed,
+          name: fileName + '.gz',
+          headers: {'content-type': 'application/gzip', 'content-length': compressed.length}
+        })
+        .then(() => console.log('Uploaded GZ.'))
+        .catch(() => console.log('GZ has failed to upload.'));
+    })()
+  ]);
+  console.log('Done.');
+};
+
+main();
