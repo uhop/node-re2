3f15c3f3e3b8dbe9598428ca66277e92b8737768
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2020-05-25 23:22:16 -0500

    Reworked dockerfile.

--- .github/actions/linux-node-10/entrypoint.sh
+++ .github/actions/linux-node-10/entrypoint.sh
@@ -1,12 +1,16 @@
 #!/bin/bash -l
 
 NVM_DIR=$HOME/.nvm
-PATH=$HOME/src/bin:$HOME/bin:$PATH
 
 curl -sS -o- https://raw.githubusercontent.com/creationix/nvm/v0.35.3/install.sh | bash
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
 nvm install --no-progress $1
+
+# nvm install --no-progress 10
+# yum install -y git
+# git clone --recursive --branch github-actions https://github.com/uhop/node-re2
+# cd node-re2
 
 npm config set unsafe-perm true
 npm ci
