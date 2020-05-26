40a6ee0668a73739622cec68774ca738bb4bbad2
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2020-05-25 23:55:28 -0500

    Updated dependencies.

--- .github/actions/linux-node-12/entrypoint.sh
+++ .github/actions/linux-node-12/entrypoint.sh
@@ -7,7 +7,7 @@ curl -sS -o- https://raw.githubusercontent.com/creationix/nvm/v0.35.3/install.sh
 
 nvm install --no-progress $1
 
-# nvm install --no-progress 10
+# nvm install --no-progress 12
 # yum install -y git
 # git clone --recursive --branch github-actions https://github.com/uhop/node-re2
 # cd node-re2
