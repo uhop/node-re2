77a3b284c1648a53848f8052b56b0d451228153e
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2020-05-25 21:35:17 -0500

    More discovery.

--- .github/actions/linux-node-10/entrypoint.sh
+++ .github/actions/linux-node-10/entrypoint.sh
@@ -1,13 +1,21 @@
 #!/bin/sh -l
 
-ldd --version
-
-echo "$HOME - $GITHUB_REPOSITORY - $GITHUB_REF"
-[ ! -z "$GITHUB_TOKEN" ] && echo "GITHUB_TOKEN is set" || echo "GITHUB_TOKEN is NOT set"
+echo "$HOME - $GITHUB_WORKSPACE - $1"
 
 ls $HOME
+ls $GITHUB_WORKSPACE
 
-# npm ci
-# npm run build --if-present
-# npm test
-# npm run create-binary-asset
+echo "Done"
+
+NVM_DIR=$HOME/.nvm
+PATH=$HOME/src/bin:$HOME/bin:$PATH
+
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+nvm install $1
+
+npm ci
+npm run build --if-present
+npm test
+npm run create-binary-asset
