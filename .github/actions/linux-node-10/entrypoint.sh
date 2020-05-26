6e88250388b02319f236e0d21788a8a372a28cac
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2020-05-25 21:51:56 -0500

    More discovery.

--- .github/actions/linux-node-10/entrypoint.sh
+++ .github/actions/linux-node-10/entrypoint.sh
@@ -2,7 +2,12 @@
 
 echo "$HOME - $GITHUB_WORKSPACE - $1"
 
+pwd
+
+echo "Home"
 ls $HOME
+
+echo "Workspace"
 ls $GITHUB_WORKSPACE
 
 echo "Done"
@@ -10,10 +15,12 @@ echo "Done"
 NVM_DIR=$HOME/.nvm
 PATH=$HOME/src/bin:$HOME/bin:$PATH
 
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+curl -sS -o- https://raw.githubusercontent.com/creationix/nvm/v0.35.3/install.sh | bash
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
 nvm install $1
+
+cd $GITHUB_WORKSPACE
 
 npm ci
 npm run build --if-present
