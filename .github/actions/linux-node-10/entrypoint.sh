40073a3d7924a07514c226a831104880d57b99ad
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2020-05-25 22:07:17 -0500

    More-or-less realistic payload.

--- .github/actions/linux-node-10/entrypoint.sh
+++ .github/actions/linux-node-10/entrypoint.sh
@@ -1,27 +1,14 @@
 #!/bin/sh -l
 
-echo "$HOME - $GITHUB_WORKSPACE - $1"
-
-pwd
-
-echo "Home"
-ls $HOME
-
-echo "Workspace"
-ls $GITHUB_WORKSPACE
-
-echo "Done"
-
 NVM_DIR=$HOME/.nvm
 PATH=$HOME/src/bin:$HOME/bin:$PATH
 
 curl -sS -o- https://raw.githubusercontent.com/creationix/nvm/v0.35.3/install.sh | bash
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-nvm install $1
+nvm install --no-progress $1
 
-cd $GITHUB_WORKSPACE
-
+npm config set unsafe-perm true
 npm ci
 npm run build --if-present
 npm test
