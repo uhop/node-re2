888500362eee7cc3d0063b9fbac0f1b36da0bd77
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2020-05-25 21:13:00 -0500

    Experimental version to understand how things work in github actions.

--- .github/actions/linux-node-10/entrypoint.sh
+++ .github/actions/linux-node-10/entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/sh -l
 
-echo "$GITHUB_REPOSITORY - $GITHUB_REF"
-[ -z "$GITHUB_TOKEN" ] && echo "GITHUB_TOKEN is set" || echo "GITHUB_TOKEN is NOT set"
+ldd --version
 
-npm ci
-npm run build --if-present
-npm test
-npm run create-binary-asset
+echo "$HOME - $GITHUB_REPOSITORY - $GITHUB_REF"
+[ ! -z "$GITHUB_TOKEN" ] && echo "GITHUB_TOKEN is set" || echo "GITHUB_TOKEN is NOT set"
+
+ls $HOME
+
+# npm ci
+# npm run build --if-present
+# npm test
+# npm run create-binary-asset
