5d7d467304f2607074487d335379af7193993a65
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2020-11-12 23:36:25 -0600

    NPM 7 has broken "npm ci" => switched to "npm i".
    Filed a bug with npm: https://github.com/npm/cli/issues/2169

--- .github/actions/linux-alpine-node-12/entrypoint.sh
+++ .github/actions/linux-alpine-node-12/entrypoint.sh
@@ -2,7 +2,7 @@
 
 npm config set unsafe-perm true
 export DEVELOPMENT_SKIP_GETTING_ASSET=true
-npm ci
+npm i
 npm run build --if-present
 npm test
 npm run save-to-github
--- .github/actions/linux-node-12/entrypoint.sh
+++ .github/actions/linux-node-12/entrypoint.sh
@@ -9,7 +9,7 @@ nvm install --no-progress $1
 
 npm config set unsafe-perm true
 export DEVELOPMENT_SKIP_GETTING_ASSET=true
-npm ci
+npm i
 npm run build --if-present
 npm test
 npm run save-to-github
