ae21ae6cc1821a353dadc24b9fc5df1503118ed8
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2021-07-03 22:41:26 -0500

    Node 15 is out.

--- .github/actions/linux-alpine-node-15/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-npm config set unsafe-perm true
-export USERNAME=`whoami`
-export DEVELOPMENT_SKIP_GETTING_ASSET=true
-npm i
-npm run build --if-present
-npm test
-npm run save-to-github
