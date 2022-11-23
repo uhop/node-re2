b880a911a7e0269d5ff00c81c1bfbcb1b298693d
Eugene Lazutkin <eugene.lazutkin@gmail.com>
2022-11-23 12:49:48 -0600

    Added Node 19 as a test and build target.

---
+++ .github/actions/linux-alpine-node-19/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+npm config set unsafe-perm true
+export USERNAME=`whoami`
+export DEVELOPMENT_SKIP_GETTING_ASSET=true
+npm i
+npm run build --if-present
+npm test
+npm run save-to-github
